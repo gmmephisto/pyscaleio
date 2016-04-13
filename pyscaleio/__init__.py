b1505fa6bff08cb631f1e4e52d5d08934515b2c6
Mikhail Ushanov <gm.mephisto@gmail.com>
2016-04-13 23:30:52 +0300

    Update top-level imports
    Signed-off-by: Mikhail Ushanov <gm.mephisto@gmail.com>

--- pyscaleio/__init__.py
+++ pyscaleio/__init__.py
@@ -1,9 +1,15 @@
 from .client import ScaleIOSession, ScaleIOClient
 from .manager import ScaleIOClientsManager
+from .models import (
+    System, ProtectionDomain, StoragePool,
+    VTree, SDC, Volume
+)
 
 __all__ = (
-    ScaleIOSession.__name__,
-    ScaleIOClient.__name__,
+    ScaleIOSession.__name__, ScaleIOClient.__name__,
+    System.__name__, ProtectionDomain.__name__,
+    StoragePool.__name__, VTree.__name__, SDC.__name__,
+    Volume.__name__
 )
 
 __version__ = "0.0.1"
